77e74f0b4d2e53ef8a3f2e147804f7bded5f4491
Mhamad-Ahmad <m_abu_g@hotmail.com>
2021-03-12 00:02:41 +0200

    fixed the google API

--- app/src/main/java/com/example/text_to_speech/SingleAlphabet.java
+++ app/src/main/java/com/example/text_to_speech/SingleAlphabet.java
@@ -2,13 +2,21 @@ package com.example.text_to_speech;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +24,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class SingleAlphabet extends AppCompatActivity {
     EditText text ;
@@ -23,10 +32,80 @@ public class SingleAlphabet extends AppCompatActivity {
     Button check ;
     Button sound ;
     private static final int RECOGNIZER_RESULT = 1;
+
+    private SpeechRecognizer speechRecognizer ;
+    private Intent  speechRecognizerIntent ;
+    String keeper = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_alphabet);
+
+        checkVoiceCommandPermission();
+        speechRecognizer =  SpeechRecognizer.createSpeechRecognizer(SingleAlphabet.this);
+        speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ar-JO");
+        //Locale.getDefault()
+
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                ArrayList<String> matchesFound = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                if(matchesFound!=null){
+                    keeper =matchesFound.get(0);
+                    Toast.makeText(SingleAlphabet.this, "Result = " + keeper, Toast.LENGTH_SHORT).show();
+                    text.setText(keeper);
+                }
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+
+
+
         TextView textView =findViewById(R.id.textView);
         text = (EditText) findViewById(R.id.text);
         button = (Button) findViewById(R.id.button);
@@ -35,17 +114,20 @@ public class SingleAlphabet extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Intent text  = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-//                text.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-//                text.putExtra(RecognizerIntent.EXTRA_PROMPT,"speech to text");
-//                startActivityForResult(text,RECOGNIZER_RESULT);
-                Intent voicerecogize = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+////                Intent text  = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+////                text.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+////                text.putExtra(RecognizerIntent.EXTRA_PROMPT,"speech to text");
+////                startActivityForResult(text,RECOGNIZER_RESULT);
+//                Intent voicerecogize = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//
+//                voicerecogize.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass().getPackage().getName());
+//                voicerecogize.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);// spilling
+//                voicerecogize.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ar-JO");
+//
+//                startActivityForResult(voicerecogize,RECOGNIZER_RESULT);
 
-                voicerecogize.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass().getPackage().getName());
-                voicerecogize.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);// spilling
-                voicerecogize.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ar-JO");
-
-                startActivityForResult(voicerecogize,RECOGNIZER_RESULT);
+                speechRecognizer.startListening(speechRecognizerIntent);
+                keeper= "" ;
 
             }
         });
@@ -86,13 +168,30 @@ public class SingleAlphabet extends AppCompatActivity {
             }
         });
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode==RECOGNIZER_RESULT && resultCode == RESULT_OK){
-            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            text.setText(matches.get(0));
-        }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        if(requestCode==RECOGNIZER_RESULT && resultCode == RESULT_OK){
+//            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//            text.setText(matches.get(0));
+//        }
+//
+//        super.onActivityResult(requestCode, resultCode, data);
 
-        super.onActivityResult(requestCode, resultCode, data);
+
+    private void checkVoiceCommandPermission(){
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(!(ContextCompat.checkSelfPermission(SingleAlphabet.this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED))
+            {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:"+getPackageName()));
+                startActivity(intent);
+                finish();
+            }
+        }
     }
+
+
+
+
+
 }
